cb211d2f53170abe64e0f452f6297d63d11b134e
Radwa  Elhosiny <radwa.elhosiny@almentor.net>
2021-06-09 14:33:42 +0200

    upload project main directories

---
+++ src/app/shared/components/page-top-banner/page-top-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-top-banner',
+  templateUrl: './page-top-banner.component.html',
+  styleUrls: ['./page-top-banner.component.scss']
+})
+export class PageTopBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
