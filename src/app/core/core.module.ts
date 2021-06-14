62b494e0d1a651d6cadb11ecb227e08c79f7d252
asmaa.elrosasy-at-486132974648 <asmaa.elrosasy@almentor.net>
2021-06-14 14:22:50 +0200

    create the footer social component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { FooterSocialComponent } from './components/footer/components/footer-social/footer-social.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, FooterSocialComponent],
   exports: [
     FooterComponent
   ],
