0fddb2cdeb166417967796e64fc3dff2a60c29dc
asmaa.elrosasy-at-486132974648 <asmaa.elrosasy@almentor.net>
2021-06-15 14:51:27 +0200

    set the direction based on the language in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnDestroy, OnInit} from '@angular/core';
+import {LanguageService} from './shared/services/language.service';
+import {Subscription} from 'rxjs';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'UNFPA-YPeer-client';
+export class AppComponent implements OnInit, OnDestroy {
+
+  private languageSubscription: Subscription;
+  isArabic = false;
+
+  constructor(private languageService: LanguageService, @Inject(DOCUMENT) private document: Document) {
+  }
+
+  ngOnInit(): void {
+    this.setDirectionBasedOnLanguage();
+  }
+
+  setDirectionBasedOnLanguage(): void {
+    this.languageSubscription = this.languageService.isArabic.subscribe((isArabic) => {
+      this.isArabic = isArabic;
+      if (isArabic) {
+        this.document.dir = 'rtl';
+        this.document.body.dir = 'rtl';
+      } else {
+        this.document.dir = 'ltr';
+        this.document.body.dir = 'ltr';
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.languageSubscription) {
+      this.languageSubscription.unsubscribe();
+    }
+  }
 }
