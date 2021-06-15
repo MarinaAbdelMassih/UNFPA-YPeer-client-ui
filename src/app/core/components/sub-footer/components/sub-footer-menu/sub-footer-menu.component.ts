04db544d7542fcb3c8bd93a7d489985e05e9d538
asmaa.elrosasy-at-486132974648 <asmaa.elrosasy@almentor.net>
2021-06-15 12:44:21 +0200

    create the sub footer component and the sub footer menu component

---
+++ src/app/core/components/sub-footer/components/sub-footer-menu/sub-footer-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-footer-menu',
+  templateUrl: './sub-footer-menu.component.html',
+  styleUrls: ['./sub-footer-menu.component.scss']
+})
+export class SubFooterMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FooterSocialComponent } from './components/footer/components/footer-social/footer-social.component';
+import { SubFooterComponent } from './components/sub-footer/sub-footer.component';
+import { SubFooterMenuComponent } from './components/sub-footer/components/sub-footer-menu/sub-footer-menu.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, FooterSocialComponent],
+  declarations: [HeaderComponent, FooterComponent, FooterSocialComponent, SubFooterComponent, SubFooterMenuComponent],
   exports: [
-    FooterComponent
+    FooterComponent,
+    SubFooterComponent
   ],
   imports: [
     CommonModule
