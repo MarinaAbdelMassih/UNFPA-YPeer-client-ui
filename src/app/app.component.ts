a0639ef8527860503ae810a0e585514f6bf44305
Radwa  Elhosiny <radwa.elhosiny@almentor.net>
2021-06-17 15:42:59 +0200

    change lang functionality updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,15 @@
-import {Component, Inject, OnDestroy, OnInit} from '@angular/core';
-import {LanguageService} from './shared/services/language.service';
-import {Subscription} from 'rxjs';
-import {DOCUMENT} from '@angular/common';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit {
 
-  private languageSubscription: Subscription;
-  isArabic = false;
-
-  constructor(private languageService: LanguageService, @Inject(DOCUMENT) private document: Document) {
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.setDirectionBasedOnLanguage();
   }
-
-  setDirectionBasedOnLanguage(): void {
-    this.languageSubscription = this.languageService.isArabic.subscribe((isArabic) => {
-      this.isArabic = isArabic;
-      if (isArabic) {
-        this.document.dir = 'rtl';
-        this.document.body.dir = 'rtl';
-      } else {
-        this.document.dir = 'ltr';
-        this.document.body.dir = 'ltr';
-      }
-    });
-  }
-
-  ngOnDestroy(): void {
-    if (this.languageSubscription) {
-      this.languageSubscription.unsubscribe();
-    }
-  }
-
 }
--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,24 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnDestroy, OnInit} from '@angular/core';
 import {LanguageService} from "../../../shared/services/language.service";
+import {DOCUMENT} from "@angular/common";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
   isArabic: boolean = false;
-  constructor(private languageService: LanguageService) { }
+  private languageSubscription: Subscription;
+
+  constructor(private languageService: LanguageService, @Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit() {
-    this.languageService.isArabic.subscribe(isArabic => {
-      this.isArabic = isArabic
-    })
+    this.setDirectionBasedOnLanguage();
+  }
+
+  setDirectionBasedOnLanguage(): void {
+    this.languageSubscription = this.languageService.isArabic.subscribe((isArabic) => {
+      this.isArabic = isArabic;
+      if (isArabic) {
+        this.document.dir = 'rtl';
+        this.document.body.dir = 'rtl';
+      } else {
+        this.document.dir = 'ltr';
+        this.document.body.dir = 'ltr';
+      }
+    });
   }
 
   changeLang(){
     this.languageService.updateLang(this.isArabic ? 'en' : 'ar');
+  }
+
+  ngOnDestroy(): void {
+    if (this.languageSubscription) {
+      this.languageSubscription.unsubscribe();
+    }
   }
 
 }
