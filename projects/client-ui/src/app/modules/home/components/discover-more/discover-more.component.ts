2a0b9cad2dc801c50bd0b8ebaa896f5020e00b84
Radwa  Elhosiny <radwa.elhosiny@almentor.net>
2021-06-23 15:25:41 +0200

    discover more section style updates

--- projects/client-ui/src/app/modules/home/components/discover-more/discover-more.component.ts
+++ projects/client-ui/src/app/modules/home/components/discover-more/discover-more.component.ts
@@ -8,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class DiscoverMoreComponent implements OnInit {
 
   items =[{img: 'assets/images/discover-more-news.png', title: 'News'},
-    {img: 'assets/images/discover-more-news.png', title: 'Stories'},
-    {img: 'assets/images/discover-more-news.png', title: 'Stories'},
-    {img: 'assets/images/discover-more-news.png', title: 'Stories'},
-    {img: 'assets/images/discover-more-news.png', title: 'Stories'}
+    {img: 'assets/images/discover-more-events.png', title: 'Events'},
+    {img: 'assets/images/discover-more-stories.png', title: 'Stories'},
+    {img: 'assets/images/discover-more-publications.png', title: 'Publications'},
+    {img: 'assets/images/discover-more-training.png', title: 'Training Manuals'}
     ];
   constructor() { }
 
