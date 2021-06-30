9aa662c04501ad1feab52a1af31b5310eab01d1e
asmaa.elrosasy-at-486132974648 <asmaa.elrosasy@almentor.net>
2021-06-30 18:17:10 +0200

    create the category item component

---
+++ src/app/shared/components/category-item/category-item.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-category-item',
+  templateUrl: './category-item.component.html',
+  styleUrls: ['./category-item.component.scss']
+})
+export class CategoryItemComponent implements OnInit {
+  @Input() hideToggle: boolean;
+  @Input() title: string;
+  @Input() count: number;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
