6f3dd77975824cf3b88ff725565c7ad9bf73c570
asmaa.elrosasy-at-486132974648 <asmaa.elrosasy@almentor.net>
2021-07-11 17:49:53 +0200

    create the resources model

---
+++ src/app/shared/models/resources.model.ts
@@ -0,0 +1,53 @@
+export interface resourcesContent {
+  resourcesList: resourcesListItem[];
+  tags: tag[];
+}
+
+export interface resourcesListItem {
+  id: number;
+  label: {AR: string, EN: string};
+  title: {AR: string, EN: string};
+  description: {AR: string, EN: string};
+  date: {AR: string, EN: string};
+  image: string;
+}
+
+export interface tag {
+  id: number;
+  name: {AR: string, EN: string};
+}
+
+export class resourcesModel implements resourcesContent{
+  resourcesList: resourcesListItem[];
+  tags: tag[];
+
+  constructor(resourcesData: any) {
+    this.resourcesList = resourcesModel.setResourcesList(resourcesData.resourcesListCollection.items);
+    this.tags = resourcesModel.setTags(resourcesData.resourcesTagsCollection.items);
+  }
+
+  private static setResourcesList(resourcesListItems: any[]): resourcesListItem[]{
+    return resourcesListItems.map((resourcesListItem) => {
+      return {
+        id: resourcesListItem.id,
+        label : {AR: resourcesListItem.labelAr, EN: resourcesListItem.labelEn},
+        title : {AR: resourcesListItem.titleAr, EN: resourcesListItem.titleEn},
+        description : {AR: resourcesListItem.descriptionAr, EN: resourcesListItem.descriptionEn},
+        date : {AR: resourcesListItem.dateAr, EN: resourcesListItem.dateEn},
+        image: resourcesListItem.image.url
+      }
+    });
+  }
+
+  private static setTags(tags: any[]): tag[] {
+    return tags.map((tag) => {
+      return {
+        id: tag.id,
+        name: {AR: tag.nameAr, EN: tag.nameEn}
+      }
+    });
+  }
+
+}
+
+
