018a6426db34d17f4f3ef2d3dd10ec5ce75d6ae7
asmaa.elrosasy-at-486132974648 <asmaa.elrosasy@almentor.net>
2021-07-12 19:16:33 +0200

    handle the style of events page in arabic

--- projects/client-ui/src/app/modules/events/events.component.ts
+++ projects/client-ui/src/app/modules/events/events.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.scss']
+  styleUrls: ['./events.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class EventsComponent implements OnInit {
   eventsList = [
