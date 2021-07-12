47c7f07e1b231404ca7f1ccfc7d1b9dff098938c
asmaa.elrosasy-at-486132974648 <asmaa.elrosasy@almentor.net>
2021-07-12 13:09:58 +0200

    integrate with contentful to get the resources data and display it

--- projects/client-ui/src/app/modules/resources/resources.component.ts
+++ projects/client-ui/src/app/modules/resources/resources.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {ResourcesResolverService} from '../../../../../../src/app/shared/services/resources-resolver.service';
+import {resourcesContent} from '../../../../../../src/app/shared/models/resources.model';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-resources',
@@ -6,72 +9,9 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./resources.component.scss']
 })
 export class ResourcesComponent implements OnInit {
-  resourcesList = [
-    {
-      id: 1,
-      label: {EN: 'Publications', AR: 'المنشورات'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      date: {EN: 'jan 12, 2021', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      image: './assets/images/latest-1.png'
-    },
-    {
-      id: 2,
-      label: {EN: 'Training manual', AR: 'دليل التدريب'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      date: {EN: 'jan 12, 2021', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      image: './assets/images/latest-1.png'
-    },
-    {
-      id: 3,
-      label: {EN: 'Publications', AR: 'المنشورات'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      date: {EN: 'jan 12, 2021', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      image: './assets/images/latest-1.png'
-    },
-    {
-      id: 4,
-      label: {EN: 'Training manual', AR: 'دليل التدريب'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      date: {EN: 'jan 12, 2021', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      image: './assets/images/latest-1.png'
-    },
-    {
-      id: 5,
-      label: {EN: 'Publications', AR: 'المنشورات'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      date: {EN: 'jan 12, 2021', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      image: './assets/images/latest-1.png'
-    },
-    {
-      id: 6,
-      label: {EN: 'Training manual', AR: 'دليل التدريب'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      date: {EN: 'jan 12, 2021', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      image: './assets/images/latest-1.png'
-    },
-    {
-      id: 7,
-      label: {EN: 'Publications', AR: 'المنشورات'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      date: {EN: 'jan 12, 2021', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      image: './assets/images/latest-1.png'
-    },
-    {
-      id: 8,
-      label: {EN: 'Training manual', AR: 'دليل التدريب'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      date: {EN: 'jan 12, 2021', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      image: './assets/images/latest-1.png'
-    }
-  ];
+
+  private subscriptions: Subscription[] = [];
+  resourcesData: resourcesContent;
 
   categoriesList = [
     {title: 'Training Manuals', count: 50, hideToggle: true},
@@ -79,19 +19,23 @@ export class ResourcesComponent implements OnInit {
     {title: 'Year', hideToggle: false},
   ];
 
-  tagsList = [
-    {id: 1, name: {EN: 'productive health', AR: 'صحة منتجة'}},
-    {id: 2, name: {EN: 'sexual health', AR: 'الصحة الجنسية'}},
-    {id: 3, name: {EN: 'maternal health', AR: 'الصحه الذهنيه'}},
-    {id: 4, name: {EN: 'gender-based violence', AR: 'العنف القائم على النوع الاجتماعي'}},
-    {id: 5, name: {EN: 'family planning', AR: 'خطة العائلة'}},
-  ];
-
-
-  constructor() {
+  constructor(private resourcesResolverService: ResourcesResolverService) {
   }
 
   ngOnInit() {
+    this.getResourcesData();
+  }
+
+  getResourcesData(): void {
+    let resourcesSub = this.resourcesResolverService.resolve().subscribe((resourcesData: resourcesContent) => {
+      this.resourcesData = undefined;
+      setTimeout(() => {
+        this.resourcesData = resourcesData;
+        console.log(resourcesData)
+      }, 200)
+
+    });
+    this.subscriptions.push(resourcesSub);
   }
 
 }
