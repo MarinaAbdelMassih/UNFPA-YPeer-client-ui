8f433cee1746264d48763d8ac136108b000c9e42
asmaa.elrosasy-at-486132974648 <asmaa.elrosasy@almentor.net>
2021-07-12 18:32:33 +0200

    handle the responsive of category item and media list

--- src/app/shared/components/category-item/category-item.component.ts
+++ src/app/shared/components/category-item/category-item.component.ts
@@ -3,7 +3,7 @@ import {Component, Input, OnInit} from '@angular/core';
 @Component({
   selector: 'app-category-item',
   templateUrl: './category-item.component.html',
-  styleUrls: ['./category-item.component.scss']
+  styleUrls: ['./category-item.component.scss'],
 })
 export class CategoryItemComponent implements OnInit {
   @Input() hideToggle: boolean;
