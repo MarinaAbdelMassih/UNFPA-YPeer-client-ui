d96d94550fa49e2828a3e8c6a7aaf3b0b0fa5b70
asmaa.elrosasy-at-486132974648 <asmaa.elrosasy@almentor.net>
2021-07-13 14:52:14 +0200

    integrate events page with contentful

--- projects/client-ui/src/app/modules/events/events.component.ts
+++ projects/client-ui/src/app/modules/events/events.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {eventsContent} from '../../../../../../src/app/shared/models/events.model';
+import {EventsResolverService} from '../../../../../../src/app/shared/services/events-resolver.service';
 
 @Component({
   selector: 'app-events',
@@ -7,90 +10,32 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class EventsComponent implements OnInit {
-  eventsList = [
-    {
-      id: 1,
-      label: {EN: 'Events', AR: 'الأحداث'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      date: {EN: 'Jan 12, 2021', AR: ''},
-      image: './assets/images/latest-1.png'
-    },
-    {
-      id: 2,
-      label: {EN: 'Events', AR: 'الأحداث'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      date: {EN: 'Jan 12, 2021', AR: ''},
-      image: './assets/images/latest-2.png'
-    },
-    {
-      id: 3,
-      label: {EN: 'Events', AR: 'الأحداث'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      date: {EN: 'Jan 12, 2021', AR: ''},
-      image: './assets/images/latest-1.png'
-    },
-    {
-      id: 4,
-      label: {EN: 'Events', AR: 'الأحداث'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      date: {EN: 'Jan 12, 2021', AR: ''},
-      image: './assets/images/latest-2.png'
-    },
-    {
-      id: 5,
-      label: {EN: 'Events', AR: 'الأحداث'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      date: {EN: 'Jan 12, 2021', AR: ''},
-      image: './assets/images/latest-1.png'
-    },
-    {
-      id: 6,
-      label: {EN: 'Events', AR: 'الأحداث'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      date: {EN: 'Jan 12, 2021', AR: ''},
-      image: './assets/images/latest-2.png'
-    },
-    {
-      id: 7,
-      label: {EN: 'Events', AR: 'الأحداث'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      date: {EN: 'Jan 12, 2021', AR: ''},
-      image: './assets/images/latest-1.png'
-    },
-    {
-      id: 8,
-      label: {EN: 'Events', AR: 'الأحداث'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      date: {EN: 'Jan 12, 2021', AR: ''},
-      image: './assets/images/latest-2.png'
-    }
-  ];
+
+  private subscriptions: Subscription[] = [];
+  eventsData: eventsContent;
 
   categoriesList = [
     {title:  'Previous Events', count: 50, hideToggle: true},
     {title: 'Upcoming Events', count: 23, hideToggle: true},
   ];
 
-  tagsList = [
-    {id: 1, name: {EN: 'productive health', AR: 'صحة منتجة'}},
-    {id: 2, name: {EN: 'sexual health', AR: 'الصحة الجنسية'}},
-    {id: 3, name: {EN: 'maternal health', AR: 'الصحه الذهنيه'}},
-    {id: 4, name: {EN: 'gender-based violence', AR: 'العنف القائم على النوع الاجتماعي'}},
-    {id: 5, name: {EN: 'family planning', AR: 'خطة العائلة'}},
-  ];
-
-  constructor() {
+  constructor(private eventsResolverService: EventsResolverService) {
   }
 
   ngOnInit() {
+    this.getEventsData();
+  }
+
+  getEventsData(): void {
+    let eventsSub = this.eventsResolverService.resolve().subscribe((eventsData: eventsContent) => {
+      this.eventsData = undefined;
+      setTimeout(() => {
+        this.eventsData = eventsData;
+        console.log(eventsData);
+      }, 200)
+
+    });
+    this.subscriptions.push(eventsSub);
   }
 
 }
