22632db542f0aec608b132e3875c5d36d2b897db
asmaa.elrosasy-at-486132974648 <asmaa.elrosasy@almentor.net>
2021-07-13 14:51:04 +0200

    create the events resolver service

---
+++ src/app/shared/services/events-resolver.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {eventsContent, EventsModel} from '../models/events.model';
+import {Observable} from 'rxjs';
+import {DataHandlerService} from './data-handler.service';
+import {EventsQuery} from '../queries/events.query';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsResolverService {
+
+  private eventsData: eventsContent;
+  constructor(private dataHandlerService: DataHandlerService) { }
+
+  resolve(): Observable<eventsContent> {
+    return new Observable<eventsContent> (subscriber=> {
+      if (this.eventsData) {
+        subscriber.next(this.eventsData);
+      } else {
+        this.dataHandlerService.getDefaultPageData(EventsQuery, 'events', (res) => {
+          return new EventsModel(res.data.events);
+        }).subscribe((eventsData: eventsContent) => {
+          this.eventsData = eventsData;
+          subscriber.next(this.eventsData);
+        },() => subscriber.next(null));
+      }
+    });
+  }
+}
