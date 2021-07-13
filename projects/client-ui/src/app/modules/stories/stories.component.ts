de0af92b0eeb236ff85d352f96f3a8ec7f0afe3f
asmaa.elrosasy-at-486132974648 <asmaa.elrosasy@almentor.net>
2021-07-13 18:50:32 +0200

    edit the category list in stories page

--- projects/client-ui/src/app/modules/stories/stories.component.ts
+++ projects/client-ui/src/app/modules/stories/stories.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {Subscription} from 'rxjs';
 import {storiesContent} from '../../../../../../src/app/shared/models/stories.model';
 import {StoriesResolverService} from '../../../../../../src/app/shared/services/stories-resolver.service';
+import {CategoryModel} from '../../../../../../src/app/shared/models/category.model';
 
 @Component({
   selector: 'app-story',
@@ -14,8 +15,8 @@ export class StoriesComponent implements OnInit {
   private subscriptions: Subscription[] = [];
   storiesData: storiesContent;
 
-  categoriesList = [
-    {title: 'Year', hideToggle: false, yearsList: [2018, 2019, 2020,2021,2022]},
+  categoriesList: CategoryModel[] = [
+    {title: {EN: 'Year', AR: 'السنه'}, hideToggle: false, yearsList: [2018, 2019, 2020,2021,2022]},
   ];
 
   constructor(private storiesResolverService: StoriesResolverService) { }
