a0fbfd63a61033542bc2870436b41852e20f7402
asmaa.elrosasy-at-486132974648 <asmaa.elrosasy@almentor.net>
2021-07-13 17:32:23 +0200

    integrate the stories page with contentful and view the data

--- projects/client-ui/src/app/modules/stories/stories.component.ts
+++ projects/client-ui/src/app/modules/stories/stories.component.ts
@@ -1,93 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {storiesContent} from '../../../../../../src/app/shared/models/stories.model';
+import {StoriesResolverService} from '../../../../../../src/app/shared/services/stories-resolver.service';
 
 @Component({
   selector: 'app-story',
   templateUrl: './stories.component.html',
-  styleUrls: ['./stories.component.scss']
+  styleUrls: ['./stories.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class StoriesComponent implements OnInit {
-  storyList = [
-    {
-      id: 1,
-      label: {EN: 'Story', AR: 'الأحداث'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      date: {EN: 'Jan 12, 2021', AR: ''},
-      image: './assets/images/latest-1.png'
-    },
-    {
-      id: 2,
-      label: {EN: 'Story', AR: 'الأحداث'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      date: {EN: 'Jan 12, 2021', AR: ''},
-      image: './assets/images/latest-2.png'
-    },
-    {
-      id: 3,
-      label: {EN: 'Story', AR: 'الأحداث'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      date: {EN: 'Jan 12, 2021', AR: ''},
-      image: './assets/images/latest-1.png'
-    },
-    {
-      id: 4,
-      label: {EN: 'Story', AR: 'الأحداث'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      date: {EN: 'Jan 12, 2021', AR: ''},
-      image: './assets/images/latest-2.png'
-    },
-    {
-      id: 5,
-      label: {EN: 'Story', AR: 'الأحداث'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      date: {EN: 'Jan 12, 2021', AR: ''},
-      image: './assets/images/latest-1.png'
-    },
-    {
-      id: 6,
-      label: {EN: 'Story', AR: 'الأحداث'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      date: {EN: 'Jan 12, 2021', AR: ''},
-      image: './assets/images/latest-2.png'
-    },
-    {
-      id: 7,
-      label: {EN: 'Story', AR: 'الأحداث'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      date: {EN: 'Jan 12, 2021', AR: ''},
-      image: './assets/images/latest-1.png'
-    },
-    {
-      id: 8,
-      label: {EN: 'Story', AR: 'الأحداث'},
-      title: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite', AR: ''},
-      description: {EN: 'Lorem ipsum dolor sit amet, consecrated advising elite, sed do emus temper incident ut labor ', AR: ''},
-      date: {EN: 'Jan 12, 2021', AR: ''},
-      image: './assets/images/latest-2.png'
-    }
-  ];
+
+  private subscriptions: Subscription[] = [];
+  storiesData: storiesContent;
 
   categoriesList = [
-    {title: 'Year', hideToggle: false},
+    {title: 'Year', hideToggle: false, yearsList: [2018, 2019, 2020,2021,2022]},
   ];
 
-  tagsList = [
-    {id: 1, name: {EN: 'productive health', AR: 'صحة منتجة'}},
-    {id: 2, name: {EN: 'sexual health', AR: 'الصحة الجنسية'}},
-    {id: 3, name: {EN: 'maternal health', AR: 'الصحه الذهنيه'}},
-    {id: 4, name: {EN: 'gender-based violence', AR: 'العنف القائم على النوع الاجتماعي'}},
-    {id: 5, name: {EN: 'family planning', AR: 'خطة العائلة'}},
-  ];
-
-  constructor() { }
+  constructor(private storiesResolverService: StoriesResolverService) { }
 
   ngOnInit() {
+    this.getStoriesData();
+  }
+
+  getStoriesData(): void {
+    let storiesSub = this.storiesResolverService.resolve().subscribe((storiesData: storiesContent) => {
+      this.storiesData = undefined;
+      setTimeout(() => {
+        this.storiesData = storiesData;
+      }, 200)
+
+    });
+    this.subscriptions.push(storiesSub);
   }
 
 }
