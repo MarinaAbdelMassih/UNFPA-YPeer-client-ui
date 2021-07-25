d130a15213bd373bb26ac3fa7c647e2f76a262d8
asmaa.elrosasy-at-486132974648 <asmaa.elrosasy@almentor.net>
2021-07-25 16:58:19 +0200

    edit the path

--- projects/client-ui/src/app/modules/course-catalog/course-catalog.module.ts
+++ projects/client-ui/src/app/modules/course-catalog/course-catalog.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CourseCatalogRoutingModule } from './course-catalog-routing.module';
-import { CourseCatalogComponent } from './course-catalog/course-catalog.component';
+import { CourseCatalogComponent } from './course-catalog.component';
+import { IllustrativeVideoSectionComponent } from './components/illustrative-video-section/illustrative-video-section.component';
+import { InstructorsListComponent } from './components/instructors-list/instructors-list.component';
+import { AdvancedCoursesSectionComponent } from './components/advanced-courses-section/advanced-courses-section.component';
 
 
 @NgModule({
-  declarations: [CourseCatalogComponent],
+  declarations: [CourseCatalogComponent, IllustrativeVideoSectionComponent, InstructorsListComponent, AdvancedCoursesSectionComponent],
   imports: [
     CommonModule,
     CourseCatalogRoutingModule
