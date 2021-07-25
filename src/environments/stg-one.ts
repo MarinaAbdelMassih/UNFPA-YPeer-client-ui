fdeb67f4888b6988e1331c86df358a9e934e9aaf
Radwa  Elhosiny <radwa.elhosiny@almentor.net>
2021-07-25 16:26:02 +0200

    add contentful token to stg env

--- src/environments/stg-one.ts
+++ src/environments/stg-one.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  contentful: {
+    spaceId: 'jvvejk00zh2l',
+    token: 'SjOnnb-PwRJ45RxLrkygZq__Tcum2HeCje-ZxqgO0c0'
+  },
+  idb: {
+    version: 1,
+    name: 'y-peer',
+    tables: ['home']
+  }
 };
