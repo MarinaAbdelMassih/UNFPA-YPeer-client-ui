151bbefbe778b73c5b4e29018b01414829b1bd2f
asmaa.elrosasy-at-486132974648 <asmaa.elrosasy@almentor.net>
2021-07-26 14:26:40 +0200

    create the empty instructor card component

---
+++ src/app/shared/components/instructor-card/instructor-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-instructor-card',
+  templateUrl: './instructor-card.component.html',
+  styleUrls: ['./instructor-card.component.scss']
+})
+export class InstructorCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
