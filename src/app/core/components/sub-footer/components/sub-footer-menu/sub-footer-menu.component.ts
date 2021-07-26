a5666c3a5712bc22f5336f9e780e4d11070eb99e
asmaa.elrosasy-at-486132974648 <asmaa.elrosasy@almentor.net>
2021-07-26 18:46:01 +0200

    fix ( https://almentor.atlassian.net/browse/YPEER-94 )

--- src/app/core/components/sub-footer/components/sub-footer-menu/sub-footer-menu.component.ts
+++ src/app/core/components/sub-footer/components/sub-footer-menu/sub-footer-menu.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-sub-footer-menu',
   templateUrl: './sub-footer-menu.component.html',
-  styleUrls: ['./sub-footer-menu.component.scss']
+  styleUrls: ['./sub-footer-menu.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SubFooterMenuComponent implements OnInit {
 
