076d2cb5f043324cfafedf0f8da9947f060d7ef0
asmaa.elrosasy-at-486132974648 <asmaa.elrosasy@almentor.net>
2021-07-26 14:26:18 +0200

    create the empty advanced courses section component

---
+++ projects/client-ui/src/app/modules/course-catalog/components/advanced-courses-section/advanced-courses-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-advanced-courses-section',
+  templateUrl: './advanced-courses-section.component.html',
+  styleUrls: ['./advanced-courses-section.component.scss']
+})
+export class AdvancedCoursesSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
