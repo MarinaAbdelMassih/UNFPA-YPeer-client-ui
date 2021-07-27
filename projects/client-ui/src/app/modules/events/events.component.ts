efae89976515f9c9539c832b12aa8b8ce168e3cc
Radwa  Elhosiny <radwa.elhosiny@almentor.net>
2021-07-27 14:27:11 +0200

    events filtration and pagination

--- projects/client-ui/src/app/modules/events/events.component.ts
+++ projects/client-ui/src/app/modules/events/events.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {Subscription} from 'rxjs';
-import {eventsContent} from '../../../../../../src/app/shared/models/events.model';
+import {eventsContent, eventsListItem} from '../../../../../../src/app/shared/models/events.model';
 import {EventsResolverService} from '../../../../../../src/app/shared/services/events-resolver.service';
 import {CategoryModel} from '../../../../../../src/app/shared/models/category.model';
 
@@ -14,6 +14,9 @@ export class EventsComponent implements OnInit {
 
   private subscriptions: Subscription[] = [];
   eventsData: eventsContent;
+  eventsList: eventsListItem[] = [];
+  showLoadMore: boolean =true;
+  selectedTag: string;
 
   categoriesList: CategoryModel[] = [
     {title: {EN: 'Previous Events', AR: 'الأحداث السابقه'}, count: 50, hideToggle: true},
@@ -28,14 +31,41 @@ export class EventsComponent implements OnInit {
   }
 
   getEventsData(): void {
-    let eventsSub = this.eventsResolverService.resolve().subscribe((eventsData: eventsContent) => {
+    let eventsSub = this.eventsResolverService.getPageData(this.eventsList.length, 2).subscribe((eventsData: eventsContent) => {
       this.eventsData = undefined;
       setTimeout(() => {
         this.eventsData = eventsData;
+        this.eventsList = this.eventsList.concat(eventsData.eventsList);
+        this.showLoadMore = this.eventsList.length < this.eventsData.eventsListTotal;
       }, 200)
 
     });
     this.subscriptions.push(eventsSub);
+  }
+
+  filterByTag(tag) {
+    this.eventsResolverService.selectedEventsTag.next(tag);
+    this.selectedTag = tag.label;
+
+    let eventsFilterSub = this.eventsResolverService.getFilteredDataByTag(tag.label).subscribe((eventsFilteredData: eventsContent) => {
+      this.eventsData = undefined;
+      setTimeout(() => {
+        this.eventsData = eventsFilteredData;
+        this.eventsList = eventsFilteredData.eventsList;
+        this.showLoadMore = false;
+        if(this.eventsResolverService.selectedEventsTag.getValue()) {
+          this.eventsData.tags.find(tag => tag.id == this.eventsResolverService.selectedEventsTag.getValue().id).selected = true;
+        }
+      }, 200)
+
+    });
+    this.subscriptions.push(eventsFilterSub);
+  }
+
+  clearData() {
+    this.selectedTag = null;
+    this.eventsList = [];
+    this.getEventsData();
   }
 
 }
