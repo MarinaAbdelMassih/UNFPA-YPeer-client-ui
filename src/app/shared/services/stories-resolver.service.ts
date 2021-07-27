338c012628ffab2bc35103d2dd319536e5ef79a6
Radwa  Elhosiny <radwa.elhosiny@almentor.net>
2021-07-27 15:17:38 +0200

    stories filtration and pagination

--- src/app/shared/services/stories-resolver.service.ts
+++ src/app/shared/services/stories-resolver.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
 import {storiesContent, StoriesModel} from '../models/stories.model';
-import {Observable} from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
 import {DataHandlerService} from './data-handler.service';
-import {StoriesQuery} from '../queries/stories.query';
+import {
+  StoriesPageQuery,
+  StoriesQuery,
+  StoriesTagsQuery,
+  StoriesYearsAndTagsQuery,
+  StoriesYearsQuery
+} from '../queries/stories.query';
+import {tag} from "../models/media.model";
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +17,8 @@ import {StoriesQuery} from '../queries/stories.query';
 export class StoriesResolverService {
 
   private storiesData: storiesContent;
+  selectedStoriesTag: BehaviorSubject<tag> = new BehaviorSubject<tag>(null);
+
   constructor(private dataHandlerService: DataHandlerService) { }
 
   resolve(): Observable<storiesContent> {
@@ -24,6 +33,58 @@ export class StoriesResolverService {
           subscriber.next(this.storiesData);
         },() => subscriber.next(null));
       }
+    });
+  }
+
+  getFilteredDataByTag(filter): Observable<storiesContent> {
+    let result;
+    return new Observable<storiesContent> (subscriber=> {
+      this.dataHandlerService.getRemoteDataWithoutSave(StoriesTagsQuery(filter), (res) => {
+        result = res;
+      }).then(() => {
+        this.storiesData = new StoriesModel({title: 'Stories', storiesListCollection: result.data.storiesListItemCollection,
+          storiesTagsCollection: result.data.storiesTagItemCollection});
+        subscriber.next(this.storiesData);
+      },() => subscriber.next(null));
+    });
+  }
+
+  getFilteredDataByYear(year): Observable<storiesContent> {
+    let result;
+    return new Observable<storiesContent> (subscriber=> {
+      this.dataHandlerService.getRemoteDataWithoutSave(StoriesYearsQuery(year), (res) => {
+        result = res;
+      }).then(() => {
+        this.storiesData = new StoriesModel({title: 'Stories', storiesListCollection: result.data.storiesListItemCollection,
+          storiesTagsCollection: result.data.storiesTagItemCollection});
+        subscriber.next(this.storiesData);
+      },() => subscriber.next(null));
+    });
+  }
+
+  getFilteredDataByYearAndTags(year, tag): Observable<storiesContent> {
+    let result;
+    return new Observable<storiesContent> (subscriber=> {
+      this.dataHandlerService.getRemoteDataWithoutSave(StoriesYearsAndTagsQuery(year, tag), (res) => {
+        result = res;
+      }).then(() => {
+        this.storiesData = new StoriesModel({title: 'Stories', storiesListCollection: result.data.storiesListItemCollection,
+          storiesTagsCollection: result.data.storiesTagItemCollection});
+        subscriber.next(this.storiesData);
+      },() => subscriber.next(null));
+    });
+  }
+
+  getPageData(skip: number, limit: number): Observable<storiesContent> {
+    let result;
+    return new Observable<storiesContent> (subscriber=> {
+      this.dataHandlerService.getRemoteDataWithoutSave(StoriesPageQuery(skip, limit), (res) => {
+        result = res;
+      }).then(() => {
+        this.storiesData = new StoriesModel({title: 'Stories', storiesListCollection: result.data.storiesListItemCollection,
+          storiesTagsCollection: result.data.storiesTagItemCollection});
+        subscriber.next(this.storiesData);
+      },() => subscriber.next(null));
     });
   }
 }
