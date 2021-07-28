049c66cf6d20831c7e316d3f54317195a1eb7e8a
asmaa.elrosasy-at-486132974648 <asmaa.elrosasy@almentor.net>
2021-07-28 18:04:22 +0200

    remove the unnecessary routes for this sprint scope

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,12 +17,6 @@ const routes: Routes = [
   },
 
   {
-    path: 'publications',
-    pathMatch: 'full',
-    loadChildren: () => import('../../projects/client-ui/src/app/modules/publications/publications.module').then(mod => mod.PublicationsModule),
-  },
-
-  {
     path: 'resources',
     pathMatch: 'full',
     loadChildren: () => import('../../projects/client-ui/src/app/modules/resources/resources.module').then(mod => mod.ResourcesModule),
@@ -46,22 +40,30 @@ const routes: Routes = [
     loadChildren: () => import('../../projects/client-ui/src/app/modules/stories/stories.module').then(mod => mod.StoriesModule),
   },
 
+  // {
+  //   path: 'publications',
+  //   pathMatch: 'full',
+  //   loadChildren: () => import('../../projects/client-ui/src/app/modules/publications/publications.module').then(mod => mod.PublicationsModule),
+  // },
+
+  // {
+  //   path: 'trainings',
+  //   pathMatch: 'full',
+  //   loadChildren: () => import('../../projects/client-ui/src/app/modules/trainings/trainings.module').then(mod => mod.TrainingsModule),
+  // }
+
   {
-    path: 'aboutUs',
+    path: 'about-us',
     pathMatch: 'full',
     loadChildren: () => import('../../projects/client-ui/src/app/modules/about-us/about-us.module').then(mod => mod.AboutUsModule),
   },
 
-  {
-    path: 'contactUs',
-    pathMatch: 'full',
-    loadChildren: () => import('../../projects/client-ui/src/app/modules/contact-us/contact-us.module').then(mod => mod.ContactUsModule),
-  },
-  {
-    path: 'trainings',
-    pathMatch: 'full',
-    loadChildren: () => import('../../projects/client-ui/src/app/modules/trainings/trainings.module').then(mod => mod.TrainingsModule),
-  }
+  // {
+  //   path: 'contactUs',
+  //   pathMatch: 'full',
+  //   loadChildren: () => import('../../projects/client-ui/src/app/modules/contact-us/contact-us.module').then(mod => mod.ContactUsModule),
+  // },
+
 ];
 
 @NgModule({
