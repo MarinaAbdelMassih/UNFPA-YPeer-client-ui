890312d097b87783e3f5b41d6666e28364d9de7e
asmaa.elrosasy-at-486132974648 <asmaa.elrosasy@almentor.net>
2021-08-16 19:59:03 +0200

    fix ( https://almentor.atlassian.net/browse/YPEER-130 )

--- src/app/shared/components/instructor-card/instructor-card.component.ts
+++ src/app/shared/components/instructor-card/instructor-card.component.ts
@@ -1,10 +1,11 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {TranslationModel} from '../../models/translation.model';
 
 @Component({
   selector: 'app-instructor-card',
   templateUrl: './instructor-card.component.html',
-  styleUrls: ['./instructor-card.component.scss']
+  styleUrls: ['./instructor-card.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class InstructorCardComponent implements OnInit {
 
