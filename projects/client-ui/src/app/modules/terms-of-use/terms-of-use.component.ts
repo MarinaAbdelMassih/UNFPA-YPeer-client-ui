6405ae9249b89f3b0094b8726c256e3fcc2fbd44
marina <marina.abdelmassih@almentor.net+1-at-486132974648>
2021-10-20 13:40:00 +0200

    terms of use integration

--- projects/client-ui/src/app/modules/terms-of-use/terms-of-use.component.ts
+++ projects/client-ui/src/app/modules/terms-of-use/terms-of-use.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {newsContent} from "../../../../../../src/app/shared/models/news.model";
+import {TermsOfUseResolverService} from "../../../../../../src/app/shared/services/terms-of-use-resolver.service";
+import {termsContent} from "../../../../../../src/app/shared/models/terms-of-use.model";
 
 @Component({
   selector: 'app-terms-of-use',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./terms-of-use.component.scss']
 })
 export class TermsOfUseComponent implements OnInit {
+  title;
+  description;
 
-  constructor() { }
+  constructor(private termsOfUseResolverService: TermsOfUseResolverService) {
+  }
 
   ngOnInit() {
+    this.getTermsData();
+  }
+
+  getTermsData(): void {
+    this.termsOfUseResolverService.resolve().subscribe((termsData: any) => {
+      this.title = termsData.title.title;
+      console.log('title', this.title);
+      this.description = termsData.description.description;
+      console.log('description', this.description);
+    });
   }
 
 }
