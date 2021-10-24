400bd502ee0c6e4fe05ef767e4248817691ab8ca
Radwa  Elhosiny <radwa.elhosiny@almentor.net>
2021-10-24 18:02:23 +0200

    add staging env parameters

--- src/environments/stg-one.ts
+++ src/environments/stg-one.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: true,
+  serviceURI: 'https://y-peer-stg-orchestration.almentor.net/api/',
+  courseId: 0,
+  drmURI: 'https://stg-drm-doroos.almentor.net/',
+  deployUrl: '',
   contentful: {
     spaceId: 'jvvejk00zh2l',
     token: 'SjOnnb-PwRJ45RxLrkygZq__Tcum2HeCje-ZxqgO0c0'
