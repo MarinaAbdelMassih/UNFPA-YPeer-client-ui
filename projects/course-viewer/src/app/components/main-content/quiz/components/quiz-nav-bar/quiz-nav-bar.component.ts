59aa64b14a051714632dcef192fe1914c6fb83a6
Radwa  Elhosiny <radwa.elhosiny@almentor.net>
2021-10-26 18:39:14 +0200

    next & previous buttons logic (not ready)

--- projects/course-viewer/src/app/components/main-content/quiz/components/quiz-nav-bar/quiz-nav-bar.component.ts
+++ projects/course-viewer/src/app/components/main-content/quiz/components/quiz-nav-bar/quiz-nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-quiz-nav-bar',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizNavBarComponent implements OnInit {
 
+  @Output() nextClicked: EventEmitter<boolean> = new EventEmitter();
+  @Output() previousClicked: EventEmitter<boolean> = new EventEmitter();
+  @Input() currentQuestionIndex;
+  @Input() numberOfQuestions;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  nextButtonClicked() {
+    this.nextClicked.emit();
+  }
+
+  previousButtonClicked() {
+    this.previousClicked.emit();
   }
 
 }
--- projects/course-viewer/src/app/components/main-content/quiz/quiz.component.ts
+++ projects/course-viewer/src/app/components/main-content/quiz/quiz.component.ts
@@ -9,9 +9,29 @@ import {IStuff} from "../../../../../../../src/app/shared/models/course-viewer/s
 export class QuizComponent implements OnInit {
 
   @Input() quiz: IStuff;
+  currentQuestion;
+  currentQuestionIndex;
+  questions = [{id:1, title: 'Question 1', body: 'question 1 body rrrrrrrr', type: 'true&false'}, {id:2, title: 'Question 2', body: 'question 2 body rrrrrrrr'}
+  ,{id:3, title: 'Question 3', body: 'question 3 body rrrrrrrr'}];
   constructor() { }
 
   ngOnInit() {
+    this.currentQuestion = this.questions[0];
+    this.currentQuestionIndex = 0;
+  }
+
+  getNextQuestion() {
+    if (this.currentQuestionIndex < this.questions.length - 1) {
+      this.currentQuestion = this.questions[this.currentQuestionIndex + 1];
+      this.currentQuestionIndex = this.currentQuestionIndex + 1;
+    }
+  }
+
+  getPreviousQuestion() {
+    if (this.currentQuestionIndex > 0) {
+      this.currentQuestion = this.questions[this.currentQuestionIndex - 1];
+      this.currentQuestionIndex = this.currentQuestionIndex - 1;
+    }
   }
 
 }
