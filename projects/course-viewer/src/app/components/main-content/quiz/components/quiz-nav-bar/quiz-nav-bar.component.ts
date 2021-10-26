f4445dff144bc94672ccbf76650b9072ee2eae36
Radwa  Elhosiny <radwa.elhosiny@almentor.net>
2021-10-26 15:58:03 +0200

    quiz - nav bar - ui

---
+++ projects/course-viewer/src/app/components/main-content/quiz/components/quiz-nav-bar/quiz-nav-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-nav-bar',
+  templateUrl: './quiz-nav-bar.component.html',
+  styleUrls: ['./quiz-nav-bar.component.scss']
+})
+export class QuizNavBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
