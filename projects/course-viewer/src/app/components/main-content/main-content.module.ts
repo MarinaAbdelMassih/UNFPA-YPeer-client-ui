4c869d88cf2108e6f1e72ce3d1f213b5daa3ba8d
Radwa  Elhosiny <radwa.elhosiny@almentor.net>
2021-10-27 12:05:30 +0200

    add the new components to main-content module

--- projects/course-viewer/src/app/components/main-content/main-content.module.ts
+++ projects/course-viewer/src/app/components/main-content/main-content.module.ts
@@ -3,19 +3,24 @@ import { CommonModule } from '@angular/common';
 import { MainContentComponent } from './main-content.component';
 import { VideoComponent } from './video/video.component';
 import {FlexModule} from "@angular/flex-layout";
+import { QuizComponent } from './quiz/quiz.component';
+import { QuizNavBarComponent } from './quiz/components/quiz-nav-bar/quiz-nav-bar.component';
+import { QuizQuestionComponent } from './quiz/components/quiz-question/quiz-question.component';
+import {MatRadioModule} from "@angular/material";
 
 
 
 @NgModule({
-  declarations: [MainContentComponent, VideoComponent],
+  declarations: [MainContentComponent, VideoComponent, QuizComponent, QuizNavBarComponent, QuizQuestionComponent],
   exports: [
     MainContentComponent
   ],
   imports: [
     CommonModule,
-    FlexModule
+    FlexModule,
+    MatRadioModule
   ],
 
-  entryComponents: [VideoComponent]
+  entryComponents: [VideoComponent, QuizComponent]
 })
 export class MainContentModule { }
