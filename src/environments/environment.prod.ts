5eeb4e0bbafee1f677870a7ab125576c05d1a1eb
Radwa  Elhosiny <radwa.elhosiny@almentor.net>
2021-11-03 15:39:27 +0200

    add links to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
   production: true,
   serviceURI: 'https://y-peer-stg-orchestration.almentor.net/api',
+  courseId: 0,
+  drmURI: 'https://stg-drm-doroos.almentor.net/',
+  deployUrl: '',
+  uploadServiceURI: 'https://adminstg-uploader.almentor.net/api/resources/',
+  cdnURI: 'https://cdn-stg-doroos.almentor.net',
   contentful: {
     spaceId: 'jvvejk00zh2l',
     token: 'SjOnnb-PwRJ45RxLrkygZq__Tcum2HeCje-ZxqgO0c0'
--- src/environments/stg-one.ts
+++ src/environments/stg-one.ts
@@ -4,6 +4,8 @@ export const environment = {
   courseId: 0,
   drmURI: 'https://stg-drm-doroos.almentor.net/',
   deployUrl: '',
+  uploadServiceURI: 'https://adminstg-uploader.almentor.net/api/resources/',
+  cdnURI: 'https://cdn-stg-doroos.almentor.net',
   contentful: {
     spaceId: 'jvvejk00zh2l',
     token: 'SjOnnb-PwRJ45RxLrkygZq__Tcum2HeCje-ZxqgO0c0'
