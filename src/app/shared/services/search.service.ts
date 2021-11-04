6238718c993ed4b05d7e9269c1137eb8360b749c
Radwa  Elhosiny <radwa.elhosiny@almentor.net>
2021-11-04 16:01:29 +0200

    search service

---
+++ src/app/shared/services/search.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+
+  constructor(private http: HttpClient) { }
+
+  getSearchData(searchWord: string) {
+    return this.http.get(`https://cdn.contentful.com/spaces/jvvejk00zh2l/entries?query=${searchWord}`,
+      {headers: {'authorization': 'Bearer SjOnnb-PwRJ45RxLrkygZq__Tcum2HeCje-ZxqgO0c0'
+        }}).toPromise()
+  }
+
+  getImageById(id: string) {
+    return this.http.get(`https://cdn.contentful.com/spaces/jvvejk00zh2l/environments/master/assets/${id}`,
+      {headers: {'authorization': 'Bearer SjOnnb-PwRJ45RxLrkygZq__Tcum2HeCje-ZxqgO0c0'
+        }}).toPromise()
+  }
+}
