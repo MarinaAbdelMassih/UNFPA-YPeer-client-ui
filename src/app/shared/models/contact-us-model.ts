4a3092c71923bafdc07d171ef1db95537500c108
marina <marina.abdelmassih@almentor.net+1-at-486132974648>
2021-11-18 18:06:34 +0200

    contact us integration

---
+++ src/app/shared/models/contact-us-model.ts
@@ -0,0 +1,10 @@
+export interface IContactUs {
+  title: string;
+  firstName: string;
+  lastName: string;
+  subject: string;
+  message: string;
+  option: string;
+  email: string;
+  response: boolean;
+}
