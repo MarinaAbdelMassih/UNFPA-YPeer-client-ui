b42b8d5d1cfabc8881ba6973626df555831c2689
asmaa.elrosasy-at-486132974648 <asmaa.elrosasy@almentor.net>
2021-12-02 16:24:51 +0200

    hide enroll button if user in waiting list

--- projects/client-ui/src/app/modules/user-profile/components/user-profile-courses/user-profile-courses.component.ts
+++ projects/client-ui/src/app/modules/user-profile/components/user-profile-courses/user-profile-courses.component.ts
@@ -32,7 +32,7 @@ export class UserProfileCoursesComponent implements OnInit {
     this.getMyCourses();
   }
 
-  getMyCourses(): void {
+  getMyCourses(userEnrolled?: boolean): void {
     this.myCoursesService.getMyCourses({userId: this.userInfo.id}).then((myCourses) => {
       this.myCourses = myCourses.courses;
       // this.myCourses =
@@ -42,12 +42,12 @@ export class UserProfileCoursesComponent implements OnInit {
       //   ];
       this.introductoryCourse = this.myCourses.find(course => course.courseType === 1);
       this.advancedCourse = this.myCourses.find(course => course.courseType === 2);
-      this.setAdvancedStatus();
+      this.setAdvancedStatus(userEnrolled);
     });
   }
 
-  setAdvancedStatus(): void {
-    if (this.introductoryCourse.progress >= 90 && this.introductoryCourse.score >= 75 && !this.advancedCourse) {
+  setAdvancedStatus(userEnrolled: boolean): void {
+    if (this.introductoryCourse.progress >= 90 && this.introductoryCourse.score >= 75 && !this.advancedCourse && !userEnrolled) {
       this.advancedStatusTitle = {EN: 'What Next?', AR: 'ماذا بعد؟'};
       this.advancedStatusDescription = {EN: 'advanced courses is available for limited time (2 months) from the date of enrollment.', AR: ''};
       this.advancedStatusBtnText = {EN: 'Enroll Now', AR: 'سجل الان'};
@@ -104,11 +104,11 @@ export class UserProfileCoursesComponent implements OnInit {
   enrollAdvanced(): void {
     //enrollment in advanced course request and recall getMyCourses() again to update the data
     this.myCoursesService.enrollAdvanced({userId: this.userInfo.id, email: this.userInfo.email, firstName: this.userInfo.firstName, lastName: this.userInfo.lastName}).then(response => {
-      if (response.status) {
-        this.getMyCourses();
-      } else {
+      this.getMyCourses(true);
+      if (!response.status) {
         this.advancedStatusTitle = {EN: 'Thank You!', AR: 'شكراً!'};
         this.advancedStatusDescription = {EN: 'You did a great job. Now you are on our waiting list, be patient; you will join the advanced course shortly', AR: ''};
+        this.advancedStatusBtnText = null;
       }
     });
   }
