ad10378800b05169bab85da283618a407eb7ae3c
radwa.elhosiny <radwa.elhosiny@486132974648>
2021-12-06 15:54:54 +0200

    call verify email endpoint

--- projects/client-ui/src/app/modules/verify-email/verify-email.component.ts
+++ projects/client-ui/src/app/modules/verify-email/verify-email.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {LanguageService} from '../../../../../../src/app/shared/services/language.service';
+import {SignInService} from "../../../../../../src/app/shared/services/sign-in.service";
 
 @Component({
   selector: 'app-verify-email',
@@ -11,9 +12,10 @@ export class VerifyEmailComponent implements OnInit {
   verifyEmailForm: FormGroup;
   isArabic: boolean;
   emailPattern = '^([a-zA-Z0-9_\\.\\-\\+])+\\@(([a-zA-Z0-9\\-])+\\.)+([a-zA-Z0-9]{2,4})+$';
+  isEmailSent: boolean;
 
 
-  constructor(private fb: FormBuilder, private languageService: LanguageService) {
+  constructor(private fb: FormBuilder, private languageService: LanguageService, private signInService: SignInService) {
     this.verifyEmailForm = this.fb.group({
         email: new FormControl('', [Validators.required, Validators.pattern(this.emailPattern)]),
       },
@@ -31,6 +33,7 @@ export class VerifyEmailComponent implements OnInit {
   }
 
   submitVerifyEmailForm() {
-    console.log(this.verifyEmailForm.value);
+    this.signInService.verifyEmail(this.verifyEmailForm.controls.email.value)
+      .then(() => this.isEmailSent = true);
   }
 }
